f07da841ba15a5bd928fb838634ecfbf2d942971
David Julian Jaramillo <davidjulianjaramillo@gmail.com>
2022-05-28 19:29:12 -0500

    Ejercios  terminados

--- Ejerciciosclase2.py
+++ Ejerciciosclase2.py
@@ -3,8 +3,7 @@
 # y el vendedor nos muestra que el precio por kilo cuesta 29,75€. ¿Crees que es correcto el precio?
 
 # En este reto tienes que hacer un programa que calcule correctamente el precio por kilo del producto.
-from copyreg import constructor
-from importlib.metadata import packages_distributions
+
 
 
 ciengramos = (5.95)
@@ -87,12 +86,12 @@ costopccondescuento = costo_pc - (costo_pc * descuento)/100
 print('El costo de la pc es de ', int(costopccondescuento), 'euros')
 ## Ejercicio 6: ¿Qué precio tenían antes del descuento?
 
-En España, las rebajas de invierno suelen comenzar entre los días 1 y 7 de enero y finalizan a final de marzo. 
-Por otro lado, las rebajas de verano empiezan durante la primera semana del mes de julio
-y finalizan durante el mes de septiembre.
+# En España, las rebajas de invierno suelen comenzar entre los días 1 y 7 de enero y finalizan a final de marzo. 
+# Por otro lado, las rebajas de verano empiezan durante la primera semana del mes de julio
+# y finalizan durante el mes de septiembre.
 
-Para aprovechar la temporada de rebajas he salido de compras. He pagado 34€ por unos
-pantalones que tenían un descuento del 15%. ¿Qué precio tenían antes de aplicar el descuento?
+# Para aprovechar la temporada de rebajas he salido de compras. He pagado 34€ por unos
+# pantalones que tenían un descuento del 15%. ¿Qué precio tenían antes de aplicar el descuento?
 
 
 ### Análisis del problema
@@ -114,6 +113,11 @@ pantalones que tenían un descuento del 15%. ¿Qué precio tenían antes de apli
 #porcentaje descuento
 
 # **¿Cómo debo responder?**  
+#Imprimir costo del producto sin descuento
+preciocondescuento = 24
+descuento1 = 15
+preciosindescuento =  ((preciocondescuento*100)/(100-descuento1))
+print ('El precio de los pantalones sin descuento es de ', preciosindescuento, 'euros')
 
 
 
